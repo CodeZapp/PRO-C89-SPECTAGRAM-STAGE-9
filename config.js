59e6ed8b66a038199cb48f4b88c5e9ab77a89b92
CodeZapp <77064508+CodeZapp@users.noreply.github.com>
2022-03-23 20:09:33 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCG4hJA9c5ZfsEtRJjMfc38zPo52-NDFlA',
+  authDomain: 'spectagram-7a804.firebaseapp.com',
+  databaseURL: 'https://spectagram-7a804-default-rtdb.firebaseio.com',
+  projectId: 'spectagram-7a804',
+  storageBucket: 'spectagram-7a804.appspot.com',
+  messagingSenderId: '793721312087',
+  appId: '1:793721312087:web:bf2fa2c33c3dfaac652412',
+};
